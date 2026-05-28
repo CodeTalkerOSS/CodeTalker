c496f2c353bc5b331fca4711ab446976bb35710b
Peter C. Allport <6022036+peteallport@users.noreply.github.com>
2026-05-27 17:31:57 -0700

    Initial Commit

---
+++ CodeTalker/CodeTalkerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodeTalkerApp.swift
+//  CodeTalker
+//
+//  Created by Peter C. Allport on 5/27/26.
+//
+
+import SwiftUI
+
+@main
+struct CodeTalkerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
